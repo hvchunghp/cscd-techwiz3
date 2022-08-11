632d7e0f2aa5dcb889fbbe84183051bd79bbcb76
HungTranXD <hungtranxd2697@gmail.com>
2022-08-11 16:13:26 +0700

    Appointment Details Component

--- src/app/components/appointment-details/appointment-details.component.ts
+++ src/app/components/appointment-details/appointment-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import appointmentDetails from "../../../assets/data/appointment-details.json"
 
 @Component({
   selector: 'app-appointment-details',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment-details.component.scss']
 })
 export class AppointmentDetailsComponent implements OnInit {
+  displayMode = "upcoming";
 
+  data = appointmentDetails;
   constructor() { }
 
   ngOnInit(): void {
